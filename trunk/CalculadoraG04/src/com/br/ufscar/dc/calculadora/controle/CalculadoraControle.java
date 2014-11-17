7be753c1925a003e5ad51766b834475737e5cd04
cleitonrqs@gmail.com <cleitonrqs@gmail.com@47f55c9f-6b7c-4e89-a3d8-67f2351dadc8>
2014-11-17 14:45:33 +0000

    Repositório Criado e código fonte adicionado.

---
+++ trunk/CalculadoraG04/src/com/br/ufscar/dc/calculadora/controle/CalculadoraControle.java
@@ -0,0 +1,108 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.ufscar.dc.calculadora.controle;
+
+/**
+ *
+ * @author Cleiton
+ */
+public class CalculadoraControle {
+    int operandoA, operandoB;
+    int resultado;
+    int estado = 0;  
+    char operacao = ' ';
+       
+    public Integer executaOperacao(){
+        switch(operacao) {
+            case '+' :
+                resultado = operandoA+operandoB;
+                break;
+            case '-' :
+                resultado = operandoA-operandoB;
+                 break;
+            case '*' : 
+                resultado = operandoA*operandoB;
+                 break;
+            case '/':
+                resultado = operandoA/operandoB;
+                
+                 break;
+                    
+        }
+        
+        return resultado;
+    }
+    
+    public void adicionaDigito(int digito){
+        switch(estado){
+            case 0 :
+                 operandoA = operandoA*10 +digito; 
+                break;
+            case 1:
+                 operandoB = operandoB*10 + digito;
+                break;
+                default:
+                    break;
+                
+        }
+           
+    }
+    public void adicionaOperador(char operador){
+       operacao = operador;
+       estado++;
+       validaEstado();
+        
+    }
+
+    public Integer getOperandoA() {
+        return operandoA;
+    }
+
+    public void setOperandoA(Integer operandoA) {
+        this.operandoA = operandoA;
+    }
+
+    public Integer getOperandoB() {
+        return operandoB;
+    }
+
+    public void setOperandoB(Integer operandoB) {
+        this.operandoB = operandoB;
+    }
+
+    public Integer getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Integer resultado) {
+        this.resultado = resultado;
+    }
+
+    public char getOperacao() {
+        return operacao;
+    }
+
+    public void setOperacao(char operacao) {
+        this.operacao = operacao;
+    }
+
+    private void validaEstado() {
+         switch(estado){
+             case 2 :
+                 executaOperacao();
+                 operandoA = resultado;
+                 operandoB = 0;
+                 resultado = 0;
+                 estado=1;
+                 break;
+             default :
+                 break;
+         }
+    }
+
+   
+    
+    
+}
