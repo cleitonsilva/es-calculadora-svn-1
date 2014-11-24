c8b7347e80de47aaa3850f7725ee927b8b55b340
cleitonrqs <cleitonrqs@47f55c9f-6b7c-4e89-a3d8-67f2351dadc8>
2014-11-24 13:47:51 +0000

    Teste

--- trunk/CalculadoraG40/src/com/br/ufscar/dc/calculadora/visao/CalculadoraFormulario.java
+++ trunk/CalculadoraG40/src/com/br/ufscar/dc/calculadora/visao/CalculadoraFormulario.java
@@ -9,12 +9,10 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author Aluno B
- * Fiz Algumas alterações aqui ... nada muito grande
-
- * @author Aluno B
- * Correção de bugs no codigo fonte do projeto calculdora
- * Data 24/11/2014
+ * @author Aluno A
+ * Não fiz Fiz Algumas alterações aqui ... nada muito grande ...
+ * Correção de bugs no codigo fonte do projeto calculdora ...
+ * Data 24/11/2014 ... verificar
  */
 public class CalculadoraFormulario extends javax.swing.JFrame {
 
